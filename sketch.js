9e8e612e606aa340526de9f7e08e84d432a37f18
Devaansh2007 <74101745+Devaansh2007@users.noreply.github.com>
2020-12-16 15:48:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,9 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-
+function preload(){
+backgroundImg = loadImage("sprites/bg.png")
+}
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
@@ -33,7 +35,7 @@ function setup(){
 }
 
 function draw(){
-    background(0);
+    background(backgroundImg);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
